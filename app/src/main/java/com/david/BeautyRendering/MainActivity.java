a093ac7a26142c67a2c682deb6793e33d51fac77
wwm <wohunsile@126.com>
2020-10-11 08:16:12 +0800

    remove not used code.

--- app/src/main/java/com/david/BeautyRendering/MainActivity.java
+++ app/src/main/java/com/david/BeautyRendering/MainActivity.java
@@ -11,19 +11,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn_texture).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TextureActivity.class));
-            }
-        });
-
-        findViewById(R.id.btn_surface).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Camera1SurfaceActivity.class));
-            }
-        });
 
         findViewById(R.id.btn_surface2).setOnClickListener(new View.OnClickListener() {
             @Override
